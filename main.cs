7b5cfeace6ef35a149a658c3cdb9d5c03fc00d8f
Simon Jones <simonroyjones@gmail.com>
2019-12-24 11:20:07 +0000

    Started to code user response

--- main.cs
+++ main.cs
@@ -2,6 +2,10 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+    Console.WriteLine("Enter the current time in NYC");
+    var time = Console.ReadLine();
+    Console.WriteLine(time);
+    int newTim = Convert.ToInt32(time);
     Console.WriteLine ("Hello New York City!");
   }
 }
