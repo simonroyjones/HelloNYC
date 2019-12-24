ecd29d616194e910ba123f1cc9f0f02ae7061eca
Simon Jones <simonroyjones@gmail.com>
2019-12-24 13:23:02 +0000

    Writing if statement

--- main.cs
+++ main.cs
@@ -2,10 +2,15 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine("Enter the current time in NYC");
+    Console.Write("Enter the current time in NYC (hh:mm): ");
     var time = Console.ReadLine();
-    Console.WriteLine(time);
-    int newTim = Convert.ToInt32(time);
-    Console.WriteLine ("Hello New York City!");
+    int colonPos = time.IndexOf(":");
+    int newTim = Convert.ToInt32(time.Substring(0,colonPos));
+    Console.WriteLine(newTim);
+    if {Convert.ToInt32(newTim) < 12}
+      Console.WriteLine ("Good Morning New York City!");
+    else
+      Console.WriteLine("Good afternoon, New York City!")
+    end
   }
 }
